297e3361a9d07be11a7a9f99e2151e8e9218be64
Danna Machi <serenita15@gmail.com>
2021-08-09 04:36:08 +1000

    add interactive form & display data list

--- webapp/app.py
+++ webapp/app.py
@@ -2,8 +2,33 @@ from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
+# get data lists
+with open('assets/common_words.json', 'r') as myfile:
+    data=myfile.read()
+common_word_list = json.loads(data)
+with open('assets/common_tags.json', 'r') as myfile:
+    data=myfile.read()
+common_tag_list = json.loads(data)
+with open('assets/common_genres.json', 'r') as myfile:
+    data=myfile.read()
+common_genre_list = json.loads(data)
+with open('assets/developer_dict.json', 'r') as myfile:
+    data=myfile.read()
+developer_dict = json.loads(data)
+with open('assets/publisher_dict.json', 'r') as myfile:
+    data=myfile.read()
+publisher_dict = json.loads(data)
+
+# get top 20 appear-the-most dev & pub
+cutdevlist = list(developer_dict.keys())
+cutdevlist = [a for a in cutdevlist if developer_dict[a] > 50]
+cutpublist = list(publisher_dict.keys())
+cutpublist = [a for a in cutpublist if publisher_dict[a] > 50]
+
+
 @app.route('/', methods=['GET'])
 def index():
-    data = json.dumps( [1.0,2.0,3.0] )
-    labels=json.dumps( ["12-31-18", "01-01-19", "01-02-19"] )
-    return render_template('index.html', data=data, labels=labels)
+    # get top 20 appear-the-most dev & pub
+    developerList = json.dumps( cutdevlist )
+    publisherList = json.dumps( cutpublist )
+    return render_template('index.html', developerList=developerList, publisherList=publisherList)
